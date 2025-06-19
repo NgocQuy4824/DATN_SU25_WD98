c46efd3e34b59abe3f7e3831e3844ca95cd01127
NgocQuy4824 <ngquy04082004@gmail.com>
2025-06-19 13:53:44 +0700

    bugfix/api-user

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,11 @@
-const UserRouter = require('./UserRouter');
+
 const ProductRouter = require('./ProductRouter');
 const TypeProductRouter = require('./TypeProductRouter');
 const SizeRouter = require('./SizeRouter');
+const UsersRoter = require('./UsersRouter')
 
 const routers = (app) => {
-    app.use('/api/user', UserRouter),
+    app.use('/api/user', UsersRoter),
     app.use('/api/product', ProductRouter);
     app.use('/api/type-product', TypeProductRouter);
     app.use('/api/size', SizeRouter);
