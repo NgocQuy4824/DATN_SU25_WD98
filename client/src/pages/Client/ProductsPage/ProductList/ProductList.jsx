55fd0b570b43ede0a075c2e61c3d5c262c85a334
NgocQuy4824 <ngquy04082004@gmail.com>
2025-07-05 21:46:27 +0700

    bugfix/product-render

--- client/src/pages/Client/ProductsPage/ProductList/ProductList.jsx
+++ client/src/pages/Client/ProductsPage/ProductList/ProductList.jsx
@@ -1,17 +1,20 @@
 import React from "react";
-import { Row, Col } from "antd";
+import { Row, Col, Button } from "antd";
 import ProductCart from "../ProductCart/ProductCart";
 
 
-export default function ProductList({products }) {
+export default function ProductList({sortedProducts,onResetFilters }) {
 
   return (
-    <Row gutter={[16, 16]}>
-      {products.map((product, idx) => (
+    <>
+    <Button style={{margin:"5px 15px"}} type="primary" onClick={onResetFilters}>Đặt lại</Button>
+    <Row style={{ margin:"10px 5px" }} gutter={[16, 16]}>
+      {sortedProducts.map((product, idx) => (
         <Col xs={24} sm={12} md={8} lg={6} key={idx}>
           <ProductCart product={product} />
         </Col>
       ))}
     </Row>
+    </>
   );
 }
